adbb8e9d3bc22ecab7eb2615866c713bd9173961
Korisnik <vmladenovic1524rn@raf.rs>
2025-11-30 02:05:46 +0100

    .

--- 4-connect/src/components/Score.jsx
+++ 4-connect/src/components/Score.jsx
@@ -10,12 +10,12 @@ function Score() {
     return (
         <div className="flex justify-center gap-12 p-4 bg-gray-100 rounded-md shadow-md">
             <div className="flex items-center gap-2 text-center">
-                <h2 className="text-lg font-semibold text-red-600">X</h2>
+                <h2 className="text-lg font-semibold text-red-600">P1</h2>
                 {xLeading && <Trophy className="w-5 h-5 text-yellow-500" />}
                 <p className="text-2xl font-bold">{score.x}</p>
             </div>
             <div className="flex items-center gap-2 text-center">
-                <h2 className="text-lg font-semibold text-blue-600">O</h2>
+                <h2 className="text-lg font-semibold text-blue-600">P2</h2>
                 {oLeading && <Trophy className="w-5 h-5 text-yellow-500" />}
                 <p className="text-2xl font-bold">{score.o}</p>
             </div>
